43d09f4fbe2000ca58859c03b5f25093aaec2b39
Raul Gutierrez <rdgutierrez@gmail.com>
2017-07-28 12:00:32 -0700

    added search bar and prop-types package

--- src/App.js
+++ src/App.js
@@ -34,7 +34,10 @@ class App extends Component {
   render() {
     return (
       <div>
-        <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts}/>
+        <ListContacts
+          onDeleteContact={this.removeContact}
+          contacts={this.state.contacts}
+        />
       </div>
     )
   }
