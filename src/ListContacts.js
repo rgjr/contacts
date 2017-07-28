84c7d419ecbff1a26ff9116454602b8bba31bde0
Raul Gutierrez <rdgutierrez@gmail.com>
2017-07-28 14:06:32 -0700

    added react router, created new contacts component, set up routes and history, finished app

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import escapeRegExp from 'escape-string-regexp';
+import sortBy from 'sort-by';
 
 class ListContacts extends Component {
   static propTypes = {
@@ -15,20 +18,49 @@ class ListContacts extends Component {
     this.setState({ query: query.trim() })
   }
 
+  clearQuery = () => {
+    this.setState({ query: '' });
+  }
+
   render() {
+    const { contacts, onDeleteContact } = this.props;
+    const { query } = this.state;
+
+    let showingContacts;
+    if (query) {
+      const match = new RegExp(escapeRegExp(query), 'i');
+      showingContacts =contacts.filter((contact) => match.test(contact.name));
+    } else {
+      showingContacts = contacts
+    }
+
+    showingContacts.sort(sortBy('name'));
+
     return (
       <div className='list-contacts'>
         <div className='list-contacts-top'>
           <input
             className='search-contacts'
             type='text'
-            placeholder='Search contacts'
-            value={this.state.query}
+            value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
+            placeholder='Search contacts'
           />
+          <Link
+            to='/create'
+            className='add-contact'
+            >Add Contact</Link>
         </div>
+
+        {showingContacts.length !== contacts.length && (
+          <div className='showing-contacts'>
+            <span>Now showing {showingContacts.length} of {contacts.length} total</span>
+            <button onClick={this.clearQuery}>Show All</button>
+          </div>
+        )}
+
         <ol className='contact-list'>
-          {this.props.contacts.map((contact) => (
+          {showingContacts.map((contact) => (
             <li key={contact.id} className='contact-list-item'>
               <div className='contact-avatar' style={{
                 backgroundImage: `url(${contact.avatarURL})`
@@ -38,7 +70,7 @@ class ListContacts extends Component {
                 <p>{contact.email}</p>
               </div>
               <button
-                onClick={() => this. props.onDeleteContact(contact)}
+                onClick={() => onDeleteContact(contact)}
                 className='contact-remove'>
                 Remove
               </button>
